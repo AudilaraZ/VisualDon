7f5da0ecf88bce75d6770723f2eb8905f525cd7b
Audrey Zeugin <audreyzeugin@gmail.com>
2020-05-08 15:45:04 +0200

    visuel

--- mon-projet/src/index.js
+++ mon-projet/src/index.js
@@ -6,18 +6,11 @@ import L from 'leaflet'
 import "billboard.js/dist/theme/insight.css";
 import bb from "billboard.js";
 
-
-//Import des bibliothèque pour le graphique bar chart race
-import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-import define from "https://api.observablehq.com/@michelle-po/bar-chart-race.js?v=3";
-
-
-
 // la carte
 const map = L.map('map').setView([50, 0], 2);
 
 // le fond de carte
-L.tileLayer('https://api.maptiler.com/maps/positron/{z}/{x}/{y}.png?key=eiKhCNn6PEp5PueYXKV5', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
     tileSize: 512,
     zoomOffset: -1,
     minZoom: 1,
@@ -57,8 +50,8 @@ function getDrugColor(d) {
                     d > 3 ? '#fe9929' :
                         d > 2 ? '#fec44f' :
                             d > 1 ? '#fee391' :
-                                d > 0.5 ? '#fff7bc' :
-                                    '#ffffe5';
+                                d > 0.5 ? '#f9eecc' :
+                                    '#fffbef';
 }
 function drugStyle(feature) {
     return {
@@ -80,7 +73,7 @@ function getTabacColor(d) {
                             d > 100 ? '#fed976' :
                                 d > 75 ? '#ffeda0' :
                                     d > 50 ? '#ffffcc' :
-                                        d > 25 ? '#fcfcef' :
+                                        d > 25 ? '#f7f7e6' :
                                             '#FFFFFF';
 }
 
@@ -104,7 +97,7 @@ function getAllColor(d) {
                             d > 100 ? '#fcbba1' :
                                 d > 75 ? '#fee0d2' :
                                     d > 59 ? '#fff5f0' :
-                                        d > 25 ? '#fcfcef' :
+                                        d > 25 ? '#f7f7e6' :
                                             '#FFFFFF';
 }
 
@@ -353,6 +346,18 @@ legendAll.onAdd = function (map) {
 // Alcool par défaut, on ajoute la légende au chargement de la carte
 legendAlcool.addTo(map)
 
+
+//////////////////////////////////////////////// TEXTE ////////////////////////////////////////////////
+
+// NE MARCHE PAS A FAIRE!!!!
+
+// const textAlcool = ;
+// textAlcool.onAdd = function (){
+//     div.innerHTML +=
+// }
+
+// textAlcool.addTo('description')
+
 ////////////////////////////////////// Generation du graphique billboard.js ////////////////////////////////
 const DATA = [
     { year: "1990" },
@@ -488,6 +493,11 @@ function generateAreaChart() {
                 alcool: "area-spline",
                 drogue: "area-spline",
             },
+            colors: {
+                tabac: "#95afca",
+                alcool: "#f8bc87",
+                drogue: "#f09c9c"
+              },
         },
         axis: {
             x: {
@@ -502,208 +512,6 @@ function generateAreaChart() {
 
 ///////////////////////////////////////////////////////// Generation du graphique Bar chart race Observable /////////////////////////////////////////////////////////////
 
-
-
-
-
-
-//   <div id="observablehq-6ef2d9cb"></div>
-// <script type="module">
-// import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
-// import define from "https://api.observablehq.com/@michelle-po/bar-chart-race.js?v=3";
-const inspect = Inspector.into("#observablehq-76018bc9");
-(new Runtime).module(define, name => (name === "chart") && inspect());
-// </script>
-
-
-data = d3.csvParse(await FileAttachment("../data/batons.csv").text(), d3.autoType)
-
-viewof replay = html`<button>Replay`;
-
-chart = {
-    replay;
-
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height]);
-
-    const updateBars = bars(svg);
-    const updateAxis = axis(svg);
-    const updateLabels = labels(svg);
-    const updateTicker = ticker(svg);
-
-    yield svg.node();
-
-    for(const keyframe of keyframes) {
-        const transition = svg.transition()
-            .duration(duration)
-            .ease(d3.easeLinear);
-
-        // Extract the top bar’s value.
-        x.domain([0, keyframe[1][0].value]);
-
-        updateAxis(keyframe, transition);
-        updateBars(keyframe, transition);
-        updateLabels(keyframe, transition);
-        updateTicker(keyframe, transition);
-
-        invalidation.then(() => svg.interrupt());
-        await transition.end();
-    }
-}
-
-duration = 60;
-n = 3;
-names = new Set(data.map(d => d.name));
-
-datevalues = Array.from(d3.rollup(data, ([d]) => d.value, d => +d.date, d => d.name))
-    .map(([date, data]) => [new Date(date), data])
-    .sort(([a], [b]) => d3.ascending(a, b))
-
-function rank(value) {
-    const data = Array.from(names, name => ({ name, value: value(name) }));
-    data.sort((a, b) => d3.descending(a.value, b.value));
-    for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
-    return data;
-}
-k = 10
-
-keyframes = {
-    const keyframes = [];
-    let ka, a, kb, b;
-    for([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
-    for (let i = 0; i < k; ++i) {
-        const t = i / k;
-        keyframes.push([
-            new Date(ka * (1 - t) + kb * t),
-            rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
-        ]);
-    }
-}
-          };
-keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
-return keyframes;
-
-nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.name);
-
-prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])));
-next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)));
-
-function bars(svg) {
-    let bar = svg.append("g")
-        .attr("fill-opacity", 0.6)
-        .selectAll("rect");
-
-    return ([date, data], transition) => bar = bar
-        .data(data.slice(0, n), d => d.name)
-        .join(
-            enter => enter.append("rect")
-                .attr("fill", color)
-                .attr("height", y.bandwidth())
-                .attr("x", x(0))
-                .attr("y", d => y((prev.get(d) || d).rank))
-                .attr("width", d => x((prev.get(d) || d).value) - x(0)),
-            update => update,
-            exit => exit.transition(transition).remove()
-                .attr("y", d => y((next.get(d) || d).rank))
-                .attr("width", d => x((next.get(d) || d).value) - x(0))
-        )
-        .call(bar => bar.transition(transition)
-            .attr("y", d => y(d.rank))
-            .attr("width", d => x(d.value) - x(0)));
-};
-
-function labels(svg) {
-    let label = svg.append("g")
-        .style("font", "bold 18px var(--sans-serif)")
-        .style("font-variant-numeric", "tabular-nums")
-        .attr("text-anchor", "end")
-        .selectAll("text");
-
-    return ([date, data], transition) => label = label
-        .data(data.slice(0, n), d => d.name)
-        .join(
-            enter => enter.append("text")
-                .attr("transform", d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
-                .attr("y", y.bandwidth() / 2)
-                .attr("x", -6)
-                .attr("dy", "-0.25em")
-                .text(d => d.name)
-                .call(text => text.append("tspan")
-                    .attr("fill-opacity", 0.7)
-                    .attr("font-weight", "normal")
-                    .attr("x", -6)
-                    .attr("dy", "1.15em")),
-            update => update,
-            exit => exit.transition(transition).remove()
-                .attr("transform", d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
-                .call(g => g.select("tspan").tween("text", d => textTween(d.value, (next.get(d) || d).value)))
-        )
-        .call(bar => bar.transition(transition)
-            .attr("transform", d => `translate(${x(d.value)},${y(d.rank)})`)
-            .call(g => g.select("tspan").tween("text", d => textTween((prev.get(d) || d).value, d.value))));
-}
-
-function textTween(a, b) {
-    const i = d3.interpolateNumber(a, b);
-    return function (t) {
-        this.textContent = formatNumber(i(t));
-    };
-};
-
-formatNumber = d3.format(",d");
-
-function axis(svg) {
-    const g = svg.append("g")
-        .attr("transform", `translate(0,${margin.top})`);
-
-    const axis = d3.axisTop(x)
-        .ticks(width / 160)
-        .tickSizeOuter(0)
-        .tickSizeInner(-barSize * (n + y.padding()));
-
-    return (_, transition) => {
-        g.transition(transition).call(axis);
-        g.select(".tick:first-of-type text").remove();
-        g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
-        g.select(".domain").remove();
-    };
-};
-
-function ticker(svg) {
-    const now = svg.append("text")
-        .style("font", `bold 70px var(--sans-serif)`)
-        .style("font-variant-numeric", "tabular-nums")
-        .attr("text-anchor", "end")
-        .attr("x", width - 6)
-        .attr("y", margin.top + barSize * (n - 0.45))
-        .attr("dy", "0.32em")
-        .text(formatDate(keyframes[0][0]));
-
-    return ([date], transition) => {
-        transition.end().then(() => now.text(formatDate(date)));
-    };
-};
-
-formatDate = d3.utcFormat("%Y");
-
-color = {
-    const scale = d3.scaleOrdinal(d3.schemeTableau10);
-    return d => scale(d.name);
-};
-
-x = d3.scaleLinear([0, 1], [margin.left, width - margin.right]);
-
-y = d3.scaleBand()
-    .domain(d3.range(n + 1))
-    .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
-    .padding(0.1);
-
-height = margin.top + barSize * n + margin.bottom;
-
-barSize = 70;
-
-margin = ({ top: 16, right: 6, bottom: 6, left: 0 });
-
-d3 = require("d3@5", "d3-array@2");
+//////// fait sur le site suivant: https://observablehq.com/@michelle-po/bar-chart-race/2
 
 
